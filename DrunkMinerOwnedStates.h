1d3e7cbff3bca8a09a6d824ce53158601c861c14
Paul <papattyn@gmail.com>
2016-09-14 21:48:22 -0400

    test

---
+++ DrunkMinerOwnedStates.h
@@ -0,0 +1,18 @@
+#ifndef DRUNK_MINER_OWNED_STATES_H
+#define DRUNK_MINER_OWNED_STATES_H
+//------------------------------------------------------------------------
+//
+//  Name:   DrunkMinerOwnedStates.h
+//
+//  Desc:   All the states that can be assigned to the Drunk Miner class
+//
+//  Author: Valryo 2016
+//
+//------------------------------------------------------------------------
+#include "State.h"
+
+class DrunkMiner;
+
+
+
+#endif
